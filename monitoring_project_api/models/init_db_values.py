41040dcea2651dcb165d3a8a806f1f16b4d935fd
Kamel Houchat <dz0@email.com>
2022-03-23 23:37:07 +0100

    #1 Add property - Add `is_http_requests_accepted` property

--- monitoring_project_api/models/init_db_values.py
+++ monitoring_project_api/models/init_db_values.py
@@ -10,7 +10,8 @@ from .task import TaskPropertyTypeEnum
 
 TASK_PROPERTIES_ID_LIST = [
     "BLACK_IP_ADDRESS_ID",
-    "AVERAGE_REQUESTS_PER_TIME_INTERVAL_ID"
+    "AVERAGE_REQUESTS_PER_TIME_INTERVAL_ID",
+    "IS_HTTP_REQUESTS_ACCEPTED_ID"
 ]
 
 TASK_ID_BY_PROPERTIES = {
@@ -30,6 +31,13 @@ TASK_PROPERTY_DEFAULT_VALUES = [
         "name": 'average_requests_per_time_interval',
         "description": "The average number of requests per time interval",
         "type": TaskPropertyTypeEnum.Dict.name,
+    },
+    {
+        "id": TASK_ID_BY_PROPERTIES['IS_HTTP_REQUESTS_ACCEPTED_ID'],
+        "name": 'is_http_requests_accepted',
+        "description": "A boolean that indicate if the http requests are "
+                       "considered as an anomalous",
+        "type": TaskPropertyTypeEnum.Boolean.name,
     }
 ]
 
@@ -52,5 +60,8 @@ TASK_PROPERTY_VALIDATOR = {
         "required": False,
         "keys": ma.fields.Int(),
         "values": ma.fields.Int()
+    },
+    TASK_ID_BY_PROPERTIES['IS_HTTP_REQUESTS_ACCEPTED_ID']: {
+        "required": False
     }
 }
