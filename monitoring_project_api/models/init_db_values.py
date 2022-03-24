4562c8723aff13e09c84e7ee1c8f6e2fe185197d
Kamel Houchat <dz0@email.com>
2022-03-24 22:39:00 +0100

    #2 Add property - Add `accepted_urls` property

--- monitoring_project_api/models/init_db_values.py
+++ monitoring_project_api/models/init_db_values.py
@@ -12,7 +12,8 @@ TASK_PROPERTIES_ID_LIST = [
     "BLACK_IP_ADDRESS_ID",
     "AVERAGE_REQUESTS_PER_TIME_INTERVAL_ID",
     "IS_HTTP_REQUESTS_ACCEPTED_ID",
-    "AVERAGE_REQUESTS_PER_CLIENT_PER_TIME_INTERVAL_ID"
+    "AVERAGE_REQUESTS_PER_CLIENT_PER_TIME_INTERVAL_ID",
+    "ACCEPTED_URLS_ID"
 ]
 
 TASK_ID_BY_PROPERTIES = {
@@ -25,20 +26,20 @@ TASK_PROPERTY_DEFAULT_VALUES = [
         "id": TASK_ID_BY_PROPERTIES['BLACK_IP_ADDRESS_ID'],
         "name": 'black_ip_address',
         "description": "A list of black IP addresses",
-        "type": TaskPropertyTypeEnum.List.name,
+        "type": TaskPropertyTypeEnum.List.name
     },
     {
         "id": TASK_ID_BY_PROPERTIES['AVERAGE_REQUESTS_PER_TIME_INTERVAL_ID'],
         "name": 'average_requests_per_time_interval',
         "description": "The average number of requests per time interval",
-        "type": TaskPropertyTypeEnum.Dict.name,
+        "type": TaskPropertyTypeEnum.Dict.name
     },
     {
         "id": TASK_ID_BY_PROPERTIES['IS_HTTP_REQUESTS_ACCEPTED_ID'],
         "name": 'is_http_requests_accepted',
         "description": "A boolean that indicate if the http requests are "
                        "considered as an anomalous",
-        "type": TaskPropertyTypeEnum.Boolean.name,
+        "type": TaskPropertyTypeEnum.Boolean.name
     },
     {
         "id": TASK_ID_BY_PROPERTIES[
@@ -46,8 +47,16 @@ TASK_PROPERTY_DEFAULT_VALUES = [
         "name": 'average_requests_per_client_per_time_interval',
         "description": "The average number of requests per client per time "
                        "interval",
-        "type": TaskPropertyTypeEnum.Dict.name,
+        "type": TaskPropertyTypeEnum.Dict.name
     },
+    {
+        "id": TASK_ID_BY_PROPERTIES['ACCEPTED_URLS_ID'],
+        "name": 'accepted_urls',
+        "description": "A list of a strict accepted urls, this property "
+                       "consider only the primary part of the URLS (without "
+                       "query arguments) ",
+        "type": TaskPropertyTypeEnum.List.name
+    }
 ]
 
 TASK_PROPERTY_VALIDATOR = {
@@ -59,11 +68,14 @@ TASK_PROPERTY_VALIDATOR = {
     #     - min_inclusive (Boolean)
     #     - max_inclusive (Boolean)
     # - required keys must have a boolean as value
+    # - items_type must have a marshmallow .fields as value (its only used
+    #   with properties of type List)
     # - keys and values must have a marshmallow.fields as value (its only used
     #   with properties of type Dict)
 
     TASK_ID_BY_PROPERTIES['BLACK_IP_ADDRESS_ID']: {
-        "required": False
+        "required": False,
+        "items_type": ma.fields.Str
     },
     TASK_ID_BY_PROPERTIES['AVERAGE_REQUESTS_PER_TIME_INTERVAL_ID']: {
         "required": False,
@@ -79,4 +91,8 @@ TASK_PROPERTY_VALIDATOR = {
         "keys": ma.fields.Int(),
         "values": ma.fields.Int()
     },
+    TASK_ID_BY_PROPERTIES['ACCEPTED_URLS_ID']: {
+        "required": False,
+        "items_type": ma.fields.Str
+    }
 }
